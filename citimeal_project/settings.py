8ba6619bc8df36ac643fa6a85e0ffea563cbba48
Neeraj Gupta <neerajcrespo@gmail.com>
2017-11-12 22:48:21 +0530

    static

--- citimeal_project/settings.py
+++ citimeal_project/settings.py
@@ -128,3 +128,31 @@ STATIC_ROOT =  'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('media')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates'),
+        ],
+
+
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader'
+            ]
+        },
+    },
+]
