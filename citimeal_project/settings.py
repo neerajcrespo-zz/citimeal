a2502299da6b4978bab1c7f12dd2efecfe822c18
Neeraj Gupta <neerajcrespo@gmail.com>
2017-11-25 20:56:10 +0530

    static files committed

--- citimeal_project/settings.py
+++ citimeal_project/settings.py
@@ -28,6 +28,8 @@ if os.environ.get("ENVIRONMENT") == "production" :
 else:
   DEBUG = True
 
+
+
 ALLOWED_HOSTS = ['citimeal.ap-south-1.elasticbeanstalk.com','citimeal.com','localhost']
 
 
@@ -124,11 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  'static'
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join('media')
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+    #'/var/www/static/',
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- citimeal_project/urls.py
+++ citimeal_project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',TemplateView.as_view(template_name='index.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
