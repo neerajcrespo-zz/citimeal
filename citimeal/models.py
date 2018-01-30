0ca5131f6e11f0f2ad87ad1886fa234bff0af7b8
amankrmeag <kaman5624@gmail.com>
2018-01-30 12:27:55 +0530

    add order models

--- citimeal/models.py
+++ citimeal/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user    = models.ForeignKey(User)
+    phone   = models.CharField(max_length=120)
+    address = models.CharField(max_length=120)
+
+    def __str__(self):
+        return str(self.user)
+
+
+class Item(models.Model):
+    item_id    = models.CharField(max_length=120)
+    item_name  = models.CharField(max_length=120, null=True, blank=True)
+    desciption = models.CharField(max_length=120)
+
+    def __str__(self):
+        return str(self.item_name)
+
+
+class Order(models.Model):
+    user  = models.ForeignKey(User)
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return str(self.user)
--- citimeal_project/settings.py
+++ citimeal_project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom app
+    'citimeal',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +80,12 @@ WSGI_APPLICATION = 'citimeal_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
